ea063d937ec967ab61ff54c99a9b0f078118cc32
Shivraj Wadkar <shivrajwadkar7@gmail.com>
2022-04-19 10:28:37 +0530

    Frist Commit

---
+++ DemoProject/src/main/java/com/qa/pageLayer/LoginPage.java
@@ -0,0 +1,11 @@
+package com.qa.pageLayer;
+
+public class LoginPage {
+	
+	public void LoginPage()
+	{
+		System.out.println("this is my Git Hub Demo Project ");
+		System.out.println("This is Login Test");
+	}
+
+}
---
+++ DemoProject/src/test/java/com/qa/testLayer/LoginTest.java
@@ -0,0 +1,10 @@
+package com.qa.testLayer;
+
+public class LoginTest {
+	
+	public void LoginTest() {
+		System.out.println("This is Login Git Hub Test ");
+		System.out.println("This is Login Test");
+	}
+
+}
